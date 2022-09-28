1eff5e631a22c894e0d7835da9129ed93598592b
janae.wel <janae.wel@gmail.com>
2022-09-28 18:13:18 -0400

    testi heroku local web

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -3,6 +3,6 @@ const path = require('path');
 // send client to the front end (client) files
 module.exports = function(app) {
     app.get('/', function(req,res) {
-        res.sendFile(path.join(__dirname, '../../client/index.html'));
+        res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
 }
